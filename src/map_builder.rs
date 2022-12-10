4c1f4d011de2429ac561cbfd07dfc7e804733c62
olidak <animatio.dev@gmail.com>
2022-12-10 16:02:25 -0600

    map_builder implementation added

--- src/map_builder.rs
+++ src/map_builder.rs
@@ -22,7 +22,7 @@ impl MapBuilder {
                 rng.range(2, 10)
             );
             let mut overlap = false;
-            for r in self.room.iter() {
+            for r in self.rooms.iter() {
                 if r.intersect(&room) {
                     overlap = true;
                 }
@@ -31,7 +31,7 @@ impl MapBuilder {
             if !overlap {
                 room.for_each(|p| {
                     if p.x > 0 && p.x < SCREEN_WIDTH && p.y > 0 && p.y < SCREEN_HEIGHT {
-                        let idx = map_idx(p.x, p.y);
+                        let idx = get_map_index_from_coords(p.x, p.y);
                         self.map.tiles[idx] = TileType::Floor;
                     }
                 });
